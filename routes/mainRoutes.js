ed342d1eabf83b7be3a3ff7b38d97910ac105b3c
holonight <faruk.oc@scope.digital>
2022-01-29 13:08:26 +0300

    Update mainRoutes.js
    main route

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mainControllers = require('../controllers/mainControllers.js');
 
+
 //Fetch All Data
 router.get("/", mainControllers.getMain);
 
