205d545241cc116b0cae839c84d1972cdc71c3c7
Eren Demir <erndemir.1@gmail.com>
2022-01-29 13:08:57 +0300

    a

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const mainControllers = require('../controllers/mainControllers.js');
 
 //Fetch All Data
-router.get("/index", mainControllers.getMain);
+router.get("/", mainControllers.getMain);
 
 module.exports = router
